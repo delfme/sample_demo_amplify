4ffdd1f798987ebb7d7abd0bbee81d0fdfe5c97d
delfme <53510751+delfme@users.noreply.github.com>
2023-11-06 14:10:42 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 
 import 'dart:io';
 import 'dart:async';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
@@ -57,7 +58,9 @@ class _MyHomePageState extends State<MyHomePage> {
     print("Fetching a sample file from the web... please wait.");
     Response response = await dio.download(
         // 2.1 MB demo file form the web
-        'https://images.unsplash.com/flagged/photo-1568164017397-00f2cec55c97?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb',
+        //'https://images.unsplash.com/flagged/photo-1568164017397-00f2cec55c97?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb',
+        // 10 MB video
+        'https://www.pexels.com/download/video/18812522/?fps=29.97&h=506&w=960',
         filePath);
     if (response.statusCode == 200){
       print("File fetched from web successfully: ${File(filePath).lengthSync()} bytes");
@@ -72,10 +75,15 @@ class _MyHomePageState extends State<MyHomePage> {
     final localStorageDir =await getApplicationDocumentsDirectory();
     String filePath = '${localStorageDir.path}/example_file.jpg';
     File file = File(filePath);
+    final len = file.lengthSync();
     var startTime = DateTime.timestamp();
-    mediaAWSUrl = await AWSStorage.uploadFile(
-      'test_amplify/${basename(file.path)})',
-      file,
+    final mediaAWSUrl = await Amplify.Storage.uploadFile(
+      key:'test_amplify/${basename(file.path)})',
+      localFile: AWSFile.fromPath(file.path),
+      onProgress: (progress) {
+        final sent = progress.fractionCompleted * len;
+        debugPrint('File upload progress %${progress.fractionCompleted} sent $sent of total $len');
+      },
     );
     debugPrint('Time elapsed to upload '
         '${DateTime.timestamp().difference(startTime).inMilliseconds}ms');
