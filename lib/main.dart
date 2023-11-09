4a67e20bb97b082ea34d65ac2bf4089975727fe5
delfme <53510751+delfme@users.noreply.github.com>
2023-11-09 09:37:50 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,13 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'aws/amplify.dart';
-import 'aws/storage.dart';
 export 'package:path_provider_platform_interface/path_provider_platform_interface.dart'
     show StorageDirectory;
 
-void main() {
-  AWSAmplify.initialize();
+
+Future<void> main() async {
+  await WidgetsFlutterBinding.ensureInitialized();
+  await AWSAmplify.initialize();
   runApp(const MyApp());
 }
 
@@ -47,23 +48,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // The returned AWS url after file upload.
-  String? mediaAWSUrl;
 
-  //Download a file from Web. It will then used to upload a file to AWS S
-  void downloadImageFromWeb() async {
+  // WRITE HERE YOUR BUCKET
+  String bucket = "YOUR_BUCKET_NAME";
+
+  // Download 2 files from Web (a 2MB image and 10MB video).
+  // They will then used to upload a file to AWS S
+  void downloadImagesFromWeb() async {
     final localStorageDir = await getApplicationDocumentsDirectory();
-    String filePath = '${localStorageDir.path}/example_file.jpg';
+    String filePath = '${localStorageDir.path}/image.jpg';
+    String filePath2 = '${localStorageDir.path}/video.mp4';
     final dio = Dio();
     print("Fetching a sample file from the web... please wait.");
+    // Download image
     Response response = await dio.download(
         // 2.1 MB demo file form the web
-        //'https://images.unsplash.com/flagged/photo-1568164017397-00f2cec55c97?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb',
-        // 10 MB video
-        'https://www.pexels.com/download/video/18812522/?fps=29.97&h=506&w=960',
+        'https://images.unsplash.com/flagged/photo-1568164017397-00f2cec55c97?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb',
         filePath);
     if (response.statusCode == 200){
       print("File fetched from web successfully: ${File(filePath).lengthSync()} bytes");
+    } else{
+      print("Couldn't fetch file from web");
+    };
+
+    // Download video
+    Response response2 = await dio.download(
+        // 10 MB video
+        'https://www.pexels.com/download/video/18812522/?fps=29.97&h=506&w=960',
+        filePath2);
+    if (response.statusCode == 200){
+      print("File fetched from web successfully: ${File(filePath2).lengthSync()} bytes");
     } else{
       print("Couldn't fetch file from web");
     }
@@ -71,52 +85,61 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
   //Upload a file to AWS
-  uploadFileToAWS() async {
+  Future<void> uploadFileToAWS(String filename) async {
     final localStorageDir =await getApplicationDocumentsDirectory();
-    String filePath = '${localStorageDir.path}/example_file.jpg';
+    String filePath = '${localStorageDir.path}/$filename';
     File file = File(filePath);
     final len = file.lengthSync();
     var startTime = DateTime.timestamp();
-    final mediaAWSUrl = await Amplify.Storage.uploadFile(
-      key:'test_amplify/${basename(file.path)})',
+    try {
+      final result = await Amplify.Storage.uploadFile(
+      key:'test_amplify/${basename(file.path)}',
       localFile: AWSFile.fromPath(file.path),
       onProgress: (progress) {
         final sent = progress.fractionCompleted * len;
-        debugPrint('File upload progress %${progress.fractionCompleted} sent $sent of total $len');
-      },
-    );
-    debugPrint('Time elapsed to upload '
-        '${DateTime.timestamp().difference(startTime).inMilliseconds}ms');
-    if (mediaAWSUrl == null) {
-      debugPrint('Failed to upload file: ${file.path}');
-    } else {
-      debugPrint(
-        'Upload success - uploaded file ${file.path}',
-      );
-    }
+        debugPrint('File upload progress %${progress
+            .fractionCompleted} sent $sent of total $len');
+      });
+      } on StorageException catch (e) {
+      safePrint('Error uploading file: $e');
+      } finally {
+      safePrint('Successfully uploaded file: ${filename}');
+      safePrint('to URL https://$bucket.s3.amazonaws.com/public/test_amplify/${filename}');
+      debugPrint('Time elapsed to upload '
+          '${DateTime.timestamp().difference(startTime).inMilliseconds}ms');
+      }
+
   }
 
 
   //download a file from AWS
   Future<void> downloadFileFromAWS(String url) async {
     final localStorageDir =await getApplicationDocumentsDirectory();
+    final dirPath = localStorageDir.path;
+    debugPrint('Download File $url');
+    final file = File('$dirPath/aws_downloads/${basename(url)}');
     var startTime = DateTime.timestamp();
-    final file = await AWSStorage.downloadFile(
-      url,
-      basename(url),
-      'aws_downloads/${basename(url)}'
-    );
-    debugPrint('Time elapsed to download '
-        '${DateTime.timestamp().difference(startTime).inMilliseconds}ms');
-    if (file == null) {
-      debugPrint('Failed to download file $url');
-    } else {
+    try {
+      final result = await Amplify.Storage.downloadFile(
+        key: url,
+        localFile: AWSFile.fromPath(file.path),
+        onProgress: (progress) {
+          final downloaded = progress.fractionCompleted;
+          debugPrint('File download progress %$downloaded');
+        },
+        options: const StorageDownloadFileOptions(
+          //  accessLevel: StorageAccessLevel.protected,
+        ),
+      ).result;
       debugPrint(
-        'Download success - downloaded file ${file.path}',
-      );
+          'Successfully downloaded file: ${result.downloadedItem.key}');
+      debugPrint('Time elapsed to download '
+          '${DateTime.timestamp().difference(startTime).inMilliseconds}ms');
+    } on StorageException catch (e) {
+      debugPrint('Error downloading file: $e');
     }
-  }
 
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -129,17 +152,34 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-                onPressed: downloadImageFromWeb,
-                child: const Text("1. Fetch an image from web and save it locally")),
+                onPressed: downloadImagesFromWeb,
+                child: const Text("1. Fetch files from web and save it locally")),
             ElevatedButton(
-                onPressed: () => uploadFileToAWS(),
-                child: const Text("2. Upload 'example_file.jpg' file to AWS")),
+                onPressed: () async {
+                  await uploadFileToAWS('image.jpg');
+                },
+                child: const Text("2. Upload 'image.jpg' file to AWS")),
             ElevatedButton(
-                onPressed: () => downloadFileFromAWS('example_file.jpg'),
-                child: const Text("8. Download 'example_file.jpg' from AWS")),
+                onPressed: () async {
+                  await uploadFileToAWS('video.mp4');
+                },
+                child: const Text("2. Upload 'video.mp4' file to AWS")),
+            ElevatedButton(
+                onPressed: () {
+                  String imageAWSUrl = 'https://$bucket.s3.amazonaws.com/public/test_amplify/image.jpg';
+                  downloadFileFromAWS(imageAWSUrl);
+                },
+                child: const Text("8. Download 'image.jpg' from AWS")),
+            ElevatedButton(
+                onPressed: () {
+                  String videoAWSUrl = 'https://$bucket.s3.amazonaws.com/public/test_amplify/video.mp4';
+                  downloadFileFromAWS(videoAWSUrl);
+                  },
+                child: const Text("8. Download 'video.mp4' from AWS")),
           ],
         ),
       ),
     );
   }
 }
+
