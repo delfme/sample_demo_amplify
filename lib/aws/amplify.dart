92bc1b174f7bb8c7b9bfe4a7f8a91ef1dcd2c286
delfme <53510751+delfme@users.noreply.github.com>
2023-11-09 09:42:30 +0100

    Add files via upload

--- lib/aws/amplify.dart
+++ lib/aws/amplify.dart
@@ -3,20 +3,27 @@ import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_storage_s3/amplify_storage_s3.dart';
 import 'package:flutter/foundation.dart';
 
-import '../../amplify_configuration.dart';
+import 'amplify_configuration.dart';
 
 class AWSAmplify {
   static Future<void> initialize() async {
     try {
-      // Add this line, to include Auth and Storage plugins.
-      await Amplify.addPlugins([
-        AmplifyAuthCognito(),
-        AmplifyStorageS3(),
-      ]);
-      // ... add other plugins, if any
-      await Amplify.configure(amplifyconfig);
+      await configureAmplify();
     } on AmplifyException catch (error) {
       debugPrint('Error configuring amplify: $error');
+    }
+  }
+
+   static Future<void> configureAmplify() async {
+    final auth = AmplifyAuthCognito();
+    final storage = AmplifyStorageS3();
+
+    try {
+      await Amplify.addPlugins([auth, storage]);
+      await Amplify.configure(amplifyconfig);
+      debugPrint('Successfully configured Amplify');
+    } on Exception catch (error) {
+      debugPrint('Something went wrong configuring Amplify: $error');
     }
   }
 }
